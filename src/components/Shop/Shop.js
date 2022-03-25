4a1dc703a77ee60ef979ebb4329281b1b67ad03b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-03-26 02:27:07 +0600

    set state and load fake data check

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,19 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
 const Shop = () => {
+    const [plants, setPlants] = useState([]);
+    useEffect( () => {
+        fetch('plants.json')
+        .then(res => res.json())
+        .then(data => setPlants(data))
+    }, [])
     return (
-        <div>
+        <div className='shop-container'>
             <div className="products-container">
-                
+                <h1>This is for Products: {plants.length}</h1>
             </div>
             <div className="cart-container">
-
+                <h2>Order Summary</h2>
             </div>
         </div>
     );
