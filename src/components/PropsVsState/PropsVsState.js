db66a17be6bf3109feab3e247e7bfcd92b1d4902
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-03-26 22:51:30 +0600

    Project Finish

--- src/components/PropsVsState/PropsVsState.js
+++ src/components/PropsVsState/PropsVsState.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './PropsVsState.css'
 
 const PropsVsState = () => {
     return (
-        <div>
+        <div className='props-vs-state'>
             <h3>What are the difference between props and state?</h3>
             <p>props is used to get data from another component. props are read-only. props can't be modified.</p>
             <p>state is used to manage internal data changes of the component. state changes can be asynchronous. state can be modified using this.setState().</p>
--- src/components/ReactWork/ReactWork.js
+++ src/components/ReactWork/ReactWork.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ReactWork.css'
 
 const ReactWork = () => {
     return (
-        <div>
+        <div className='react-work'>
             <h3>How react works?</h3>
             <p>React flows one way binding data or unidirectional data. It connects parent and child components, render them and fix bug very quickly. React creats a Virtual DOM (VDOM) that is a programming concept that keeps a version close to the real dom in memory, so that it can compare which part of the UI has changed, then change the UI accordingly. It takes a diff algorithm to understand which part has changed.</p>
         </div>
